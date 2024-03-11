68cbc45043230360688157de0521ddcb81f1263f
Rudr <rudrprasad@yahoo.com>
2024-03-11 15:18:01 +1200

    trying to upload 2

--- src/app/(admin)/(nav)/admin/[userId]/tags/[id]/page.tsx
+++ src/app/(admin)/(nav)/admin/[userId]/tags/[id]/page.tsx
@@ -1,12 +1,9 @@
-import { GetCategoryById } from "@/actions/category";
-
-import EditCategoryForm from "@/components/EditCategoryForm";
-
 const Page = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
-  const category = await GetCategoryById(id);
+  // const category = await GetCategoryById(id);
 
-  return <>{category && <EditCategoryForm category={category} />}</>;
+  // return <>{category && <EditCategoryForm category={category} />}</>;
+  return <></>;
 };
 
 export default Page;
